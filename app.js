96e827bf4bfd0ccdba3fc1f954ee862bc2a507bf
bhavishyayarapathineni <bhavishyayarapathineni123@gmail.com>
2022-04-06 16:44:07 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var OrangefruitsRouter = require('./routes/Orangefruits');
+var OraganicfruitsRouter = require('./routes/Oraganicfruits');
 var addmodsRouter = require('./routes/addmods');
 var selectorRouter = require('./routes/selector');
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Orangefruits', OrangefruitsRouter);
+app.use('/Oraganicfruits', OraganicfruitsRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 
